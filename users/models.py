17d86b74b08f54986de77871516d549f028b2fe6
KimEunSu <misili17114@gmail.com>
2023-05-08 21:03:35 +0900

    creat model review

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class User():
+    pass
+
+
+class Review():
+    # room
+    user = models.ForeignKey() #나중에 추가 
+    # booked
+    title = models.CharField()
+    content = models.TextField()
+    stars = models.IntegerChoices([1, 2, 3, 4, 5] , null=True)
+    create_at = models.DateTimeField(auto_now_add=True)
