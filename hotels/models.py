b07425c3e6511b939cc1b6a972612a0d649bcec6
Msgun7 <fgarr@naver.com>
2023-05-08 20:59:32 +0900

    Make_book_models

--- hotels/models.py
+++ hotels/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django import reverse
 
-# Create your models here.
+class Book(models.Model):
+    # user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    # room = models.ForeignKey(Rooms, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    members = models.IntegerField(default=1)
+    check_in = models.DateField()
+    check_out = models.DateField()
+    
+    # def __str__(self):
+    #     return self.user
