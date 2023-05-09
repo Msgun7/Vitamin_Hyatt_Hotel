a46dbf1978b8c8d483c71529cede7da9df97d528
Msgun7 <fgarr@naver.com>
2023-05-09 11:42:51 +0900

    makes

---
+++ hotels/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Rooms
+
+
+
+class RoomsSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Rooms
+        fields = '__all__'
+
+# 만들었다. 시리얼라이저 했다. 내가
