608394a2f6ec7002a07882c23895e2c672fa0340
KimEunSu <misili17114@gmail.com>
2023-05-09 10:02:15 +0900

    test

--- users/models.py
+++ users/models.py
@@ -8,9 +8,12 @@ class User():
 
 class Review():
     # room
-    user = models.ForeignKey() #나중에 추가 
+    user = models.ForeignKey(User, on_delete=models.CASCADE) #나중에 추가 
     # booked
-    title = models.CharField()
+    title = models.CharField(max_length=100)
     content = models.TextField()
     stars = models.IntegerChoices([1, 2, 3, 4, 5] , null=True)
     create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
