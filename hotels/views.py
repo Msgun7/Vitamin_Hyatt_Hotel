a8e736b594bfcd257d73816bac6236df0a9f1391
miyeong <kmy9810@naver.com>
2023-05-09 10:59:15 +0900

    add Bookmodel, views

--- hotels/views.py
+++ hotels/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+class RoomViewAPI():
+    pass
+
+
+class DetailRoomViewAPI():
+    pass
+
+
+class BookUsersViewAPI():
+    pass
+
+
+class SpotViewAPI():
+    pass
