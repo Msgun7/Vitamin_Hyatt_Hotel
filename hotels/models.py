51414cdb46b4b28a593818f69f72d4106ef81a6e
KimEunSu <misili17114@gmail.com>
2023-05-10 14:43:17 +0900

    backup

--- hotels/models.py
+++ hotels/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from users.models import User
+from users.models import User 
 
 class Spots(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -9,9 +9,6 @@ class Spots(models.Model):
 
     def __str__(self):
         return self.name
-
-
-
 
 
 # null, blank
