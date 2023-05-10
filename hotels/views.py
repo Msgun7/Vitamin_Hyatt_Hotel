392e5b2e386db4c3f0b94aa0d131ca308c03c2cd
miyeong <kmy9810@naver.com>
2023-05-10 11:08:36 +0900

    fix hetels.view

--- hotels/views.py
+++ hotels/views.py
@@ -37,6 +37,7 @@ class DetailRoomViewAPI(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
     def delete(self, request, room_id):
         room = self.get_object(request, room_id)
         room.delete()
@@ -115,4 +116,46 @@ class BookManage(APIView):
 
 class ReserView(APIView):
     pass
+
+
+
+# 방 정보 수정 및 삭제
+class DetailRoomViewAPI(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def get_object(self, request, room_id):
+        room = get_object_or_404(Rooms, id=room_id, user=request.user)
+        return room
+
+    def put(self, request, room_id):
+        room = self.get_object(request, room_id)
+        serializer = DetailSerializer(room, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, room_id):
+        pass
+
+
+class BookUsersViewAPI(APIView):
+    def get(self, request, room_id):
+        booked_all_rooms = get_object_or_404(Book, id=room_id)
+        serializer = RoomsSerializer(booked_all_rooms, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class SpotViewAPI(APIView):
+    def get(self, request):
+        spots = Spots.objects.all()
+        serializer = SpotSerializer(spots, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = SpotSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
