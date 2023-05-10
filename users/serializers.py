63a2648d828ef824ac657b64fa9bb1800882077c
Yang ye lin <oky07031217@gmail.com>
2023-05-10 11:28:40 +0900

    update users app

--- users/serializers.py
+++ users/serializers.py
@@ -3,14 +3,23 @@ from users.models import User, BasicUser
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class UserSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True, required=True)
+    password2 = serializers.CharField(write_only=True, required=True)
+    
     class Meta:
         model = User
         fields = '__all__'
         
+    def validate(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError("The two password fields didn't match.")
+        return data
+        
     def create(self,validated_data):
-        user = super().create(validated_data)
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
         password = user.password
-        user.set_password(password)
+        user.set_password(validated_data['password1'])
         user.save()
         return user
 
