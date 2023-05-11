887951a1aba60375a7b54d73aaf4aff7bc6b6614
Msgun7 <fgarr@naver.com>
2023-05-11 17:37:43 +0900

    유효성 검사후 pull 2

--- hotels/serializers.py
+++ hotels/serializers.py
@@ -1,17 +1,35 @@
 from rest_framework import serializers
+
 from rest_framework.serializers import ValidationError
 from .models import Rooms, Book, Spots
 from .validators import check_existing_room
 
 
 class RoomsSerializer(serializers.ModelSerializer):
+
+    def check_existing_room(**kwargs):
+        # if len(attrs['max_members']) < 0:
+        #     raise ValidationError("인원이 0보다 작을 수 없습니다!")
+        existing_room = Rooms.objects.filter(
+            spot=kwargs['spot'],
+            name=kwargs['name'],
+
+        ).exists()
+        if existing_room:
+            return True
+
     class Meta:
         model = Rooms
         fields = '__all__'
 
     def validate(self, attrs):
+
         if attrs['max_members'] < 0:
             raise ValidationError('인원은 1인 이상부터 가능 합니다!')
+
+        if attrs['max_members'] < 0 or attrs['max_members'] > 10:
+            raise ValidationError('인원은 1인 이상부터 가능 합니다! 최대 인원은 10명까지입니다.')
+
         if check_existing_room(name=attrs['name'], spot=attrs['spot']):
             raise ValidationError('이미 존재 하는 Room입니다!')
         return attrs
@@ -35,6 +53,7 @@ class DetailSerializer(serializers.ModelSerializer):
 
 
 class SpotSerializer(serializers.ModelSerializer):
+
     all_room = serializers.SerializerMethodField()
     # total_room2 = DetailSerializer(many=True, read_only=True)
 
@@ -57,3 +76,4 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta():
         model = Book
         fields = '__all__'
+
--- hotels/urls.py
+++ hotels/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path, include
 from hotels import views
+from Vitamin_Hyatt import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
     path('book/',views.BookManage.as_view(), name='book'),
     path('rooms/', views.RoomView.as_view(), name='rooms_view'),
+
     path('rooms/spot/', views.SpotViewAPI.as_view(), name='spot_view'),
     path('rooms/spot/<int:spot_id>', views.SpotViewAPI.as_view(), name='spot_detail_view'),
     path('rooms/<int:room_id>/', views.DetailRoomViewAPI.as_view(), name='detail_room_view'),
     path('customers/<int:room_id>/', views.BookUsersViewAPI.as_view()),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
