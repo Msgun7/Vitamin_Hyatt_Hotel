f6852ab6de64075087a087aec754a0d9b893d56a
miyeong <120363383+kmy9810@users.noreply.github.com>
2023-05-15 01:28:00 +0900

    Update urls.py

--- Vitamin_Hyatt/urls.py
+++ Vitamin_Hyatt/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('manager/', include('hotels.urls')),
     path('reviews/', include('reviews.urls')),
-    path('cal/', include('hotels.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
